6ce5006b15c8f78992ea67631cbc402bedc7f2bd
jay <jaypifiverr@gmail.com>
2022-12-21 11:35:04 -0500

    commit tree 68f5212e582c7025f4ea70666f3410d9c3627e1e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,50 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
-  end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
+    if self.end_with?(".")
+      return true
+    else
+      return false 
   end
 end
+
+def question?
+  if self.end_with?("?")
+    return true
+    else
+      return false
+    end
+  end
+  
+  def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
+  end
+  
+  def count_sentences
+    sente = self.split(/[.?!+]/)
+    sente.reject!(&:empty?)
+    sente.count
+  #   num = 0
+  #   words = self.split(" ")
+  #   return words.length
+    
+  #   words.collect do |name|
+  #     # puts name
+  #     name.end_with?("." || "!" || "?").count
+  #   end
+  # return num
+end
+
+end
+binding.pry
+
+# s1 = String.new('rose', 3.00)
+
+word = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+word.count_sentences
